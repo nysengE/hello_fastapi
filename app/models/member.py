228267d1e54eedfc737eca54aa2a8f9e1bedb4c8
Frontends <dytpq10@gmail.com>
2024-08-14 13:39:14 +0900

    sqlalchemy - dbfactory를 비동기 컨텍스트 매니저로 실행

--- app/models/member.py
+++ app/models/member.py
@@ -1,16 +1,12 @@
 from datetime import datetime
-
-from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, DateTime
 
 from app.models.base import Base
 
-
 class Member(Base):
     __tablename__ = 'member'
 
-    mno = Column(Integer,
-                 primary_key=True, autoincrement=True, index=True)
+    mno = Column(Integer, primary_key=True, autoincrement=True, index=True)
     userid = Column(String, index=True)
     passwd = Column(String)
     name = Column(String)
